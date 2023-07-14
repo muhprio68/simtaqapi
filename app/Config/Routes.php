421a048d117349cbf08f0feddf0b205ff58a9e56
muhammadprio93 <muhammadprio93@student.ub.ac.id>
2023-07-14 15:41:17 +0700

    fix ganti sandi4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -49,7 +49,7 @@ $routes->resource('saldo');
 $routes->resource('charge');
 $routes->resource('nomor');
 $routes->resource('user');
-$routes->post('gantipassword', 'GantiPassword');
+$routes->put('gantipassword', 'GantiPassword');
 $routes->post('notifikasi', 'Notifikasi::index');
 //$routes->resource('me');
 
